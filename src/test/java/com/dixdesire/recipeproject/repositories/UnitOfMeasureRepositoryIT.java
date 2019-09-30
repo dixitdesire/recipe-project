b7570d01f120e35d86dc3db074d5adb6ed90873e
dixdesire <dixitwadhwani@gmail.com>
2019-09-30 13:45:03 +0530

    -- Added the IT test and fail-safe plugin in pom.xml

---
+++ src/test/java/com/dixdesire/recipeproject/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,38 @@
+package com.dixdesire.recipeproject.repositories;
+
+import com.dixdesire.recipeproject.model.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByUom() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional =unitOfMeasureRepository.findByUom("Tablespoon");
+        assertEquals("Tablespoon", unitOfMeasureOptional.get().getUom());
+    }
+
+    @Test
+    public void findByUomCup() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional =unitOfMeasureRepository.findByUom("Cup");
+        assertEquals("Cup", unitOfMeasureOptional.get().getUom());
+    }
+
+}
